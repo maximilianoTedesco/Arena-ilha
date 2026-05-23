b447dff18499a4564ee6385c0916e1cbe44c20b5
maxytedesco <102389819+maximilianoTedesco@users.noreply.github.com>
2026-05-22 21:51:26 -0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://sjfsonndmtgoskmuazum.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_aNPAIzX4LNRhX64YMkXtmg_ruRpvy0G";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
